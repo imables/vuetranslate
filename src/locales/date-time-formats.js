0d596cee2112146f5a729b16e006fae91b14fb8e
imables <61050140+imables@users.noreply.github.com>
2021-03-09 19:46:33 +0000

    Add files via upload

---
+++ src/locales/date-time-formats.js
@@ -0,0 +1,11 @@
+const dateTimeFormats = {
+  'en-US': {
+    short: { year: "numeric", month: "short", day: "numeric" }
+  },
+  'ar-EG': {
+    short: { year: "numeric", month: "long", day: "numeric" }
+  }
+}
+
+
+export default dateTimeFormats
